068674087eec35a7a740504f48d35618209a74ac
Angel <areyes-2019092@kinal.edu.gt>
2024-03-08 19:47:56 -0600

    Hice el server.js

--- config/server.js
+++ config/server.js
@@ -4,4 +4,51 @@ import morgan from "morgan";
 import  express  from "express";
 import helmet from "helmet";
 import cors from "cors";
+import Producto from "../src/producto/producto.routes.js";
+import Usuario from "../src/usuario/usuario.routes.js";
+import Categoria from "../src/categoria/categoria.routes.js";
+import { dbConnection } from "./mongo.js";
+class Server {
+    constructor() {
+      this.app = express();
+      this.port = process.env.PORT;
+      this.usuarioPath = "/tiendaOnline/v1/usuarios";
+      this.categoriaPath = "/tiendaOnline/v1/categorias";
+      this.productoPath = "/tiendaOnline/v1/productos";
+  
+      this.middlewares();
+      this.conectarDB();
+      this.routes();
+    }
+  
+    async conectarDB() {
+      await dbConnection();
+    }
+  
+    middlewares() {
+      this.app.use(
+        express.urlencoded({
+          extended: false,
+        })
+      );
+      this.app.use(cors());
+      this.app.use(express.json());
+      this.app.use(helmet());
+      this.app.use(morgan("dev"));
+    }
+  
+    routes() {
+      this.app.use(this.usuarioPath, Usuario);
+      this.app.use(this.categoriaPath, Categoria);
+      this.app.use(this.productoPath, Producto);
+    }
+  
+    listen() {
+      this.app.listen(this.port, () => {
+        console.log("Servidor ejecutandose en puerto", this.port);
+      });
+    }
+  }
 
+  
+export default Server;
